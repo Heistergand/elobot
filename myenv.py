d6b3713e209f59cb1e551931c367e3e5594a6e9c
Heistergand <info@heistergand.com>
2020-05-15 18:43:42 +0200

    Create myenv.py
    File to set project environment variables.

---
+++ myenv.py
@@ -0,0 +1,27 @@
+# config.py
+
+# Remember: 
+# Do not make this file public, after you have set your secret values.
+
+# Logging
+# -------
+# This is optional. If ommited, defaults to warning. 
+# Valid values: [ info | debug ] 
+LOGLEVEL = 'info'
+
+# Discord settings
+# ----------------
+# Your secret discord token 
+# DISCORD_TOKEN = ''
+DISCORD_TOKEN = ''
+
+# Trigger that the bot will react to in your channels. 
+# DISCORD_TRIGGER = '!elo'
+DISCORD_TRIGGER = '!elo'
+
+# IRC (reserved for future use)
+TMI_TOKEN = 'oauth:'
+CLIENT_ID = ''
+BOT_NICK = ''
+BOT_PREFIX = '!'
+CHANNEL = ''
